dec407a889167c914bf73191d264e128e0e6cb3e
evgeny <evgeny@MacBook-Air-evgeny.local>
2021-12-28 03:40:34 +0300

    small fix

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def create_app(foo):
 
 
 if __name__ == "__main__":
-    host = os.environ.get("HOST", "127.0.0.1")
+    host = os.environ.get("HOST", "localhost")
     app.run(host=host, port=8000)
